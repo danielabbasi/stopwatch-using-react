5ab8d3c99c683c2c456efcde47c7986892f9b4f7
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-09 09:25:24 +0100

    Created a simple stopwatch class which I will later export into App.js

---
+++ stopwatch/src/Stopwatch.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Stopwatch extends Component {
+    render() {
+        return (
+            <div>
+            <p>0ms</p>
+            <button>Start</button>
+            <button>Reset</button>
+            </div>
+        )
+    }
+    }
