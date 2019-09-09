389b971bbe6752d94bb2d9afbc3d16454d242f1a
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-09 18:03:56 +0100

    doing some refactoring - WIP

--- stopwatch/src/Stopwatch.js
+++ stopwatch/src/Stopwatch.js
@@ -1,67 +1,90 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Stopwatch extends Component {
     constructor(props) {
         super(props);
-        this.state = {status: false,
-                      timeElapsed: 0}
+        this.state = {
+            status: false,
+            timeElapsed: 0,
+            laps: [1, 2, 8]
+        }
     }
-    handleStopStart = () => {
-        this.setState(state => {
-            if (state.status) {
-                clearInterval(this.timer); // stops the timer when stop button is pressed
-            } else {
-                const startTime = Date.now() - this.state.timeElapsed;
-                console.log(startTime);
-                this.timer = setInterval(() => {
-                    this.setState({ timeElapsed: Date.now() - startTime }); // updater function timer that accepts the current state and returns new state with updated time. this is because state is immutable
-                });
-            }
-            return {status: !state.status};
-        })
-    }
+    // handleStopStart = () => {
+    //     this.setState(state => {
+    //         if (state.status) {
+    //             clearInterval(this.timer); // stops the timer when stop button is pressed
+    //         } else {
+    //             // const startTime = Date.now() - this.state.timeElapsed;
+    //             // console.log(startTime);
+    //             this.timer = setInterval(() => {
+    //                 this.setState({ timeElapsed: state.timeElapsed++ }); // updater function timer that accepts the current state and returns new state with updated time. this is because state is immutable
+    //             },10);
+    //         }
+    //         return {status: !state.status};
+    //     })
+    // }
 
-    handleLapReset = () => {
-        clearInterval (this.timer);
-        this.setState({ timeElapsed : 0, status: false });
-    }
+    // handleLapReset = () => {
+    //     this.setState( state => {
+    //         if (state.status === false) {
+    //             clearInterval(this.timer);
+    //             this.timer = this.setState({ timeElapsed : 0});
+    //         } else {
+
+
+    //         }
+    //     })
+    // }
 
     millisecondConversion = (timeElapsed) => {
-        var milliseconds = timeElapsed % 1000;
-        var seconds = Math.floor((timeElapsed / 1000) % 60);
-        var minutes = Math.floor((timeElapsed / (60 * 1000)) % 60);
+        var milliseconds = timeElapsed % 100;
+        var seconds = Math.floor((timeElapsed / 100) % 60);
+        var minutes = Math.floor((timeElapsed / (60 * 100)) % 60);
         const pad = (time) => time < 10 ? '0' + time : time
-        
+
         return pad(minutes) + ":" + pad(seconds) + "." + pad(milliseconds);
     }
 
-    // componentDidMount() {
-    //     this.timerID = setInterval(
-    //         () => this.tick(),
-    //         1000
-    //     );
+    // findDifference = (array) => {
+    //     return array.map(function(value, index) {
+    //         if (index === 0) {
+    //             return value
+    //         } else {
+    //             return value - array[index-1];
+    //         }
+    //     })
     // }
 
-    // tick() {
-    //     this.setState({
-    //         timeElapsed: this.timeElapsed + 1
-    //     });
-    // }
+    // // componentDidMount() {
+    // //     this.timerID = setInterval(
+    // //         () => this.tick(),
+    // //         1000
+    // //     );
+    // // }
 
-    componentWillUnmount() {
-        clearInterval(this.timer);
-    }
+    // // tick() {
+    // //     this.setState({
+    // //         timeElapsed: this.timeElapsed + 1
+    // //     });
+    // // }
+
+    // componentWillUnmount() {
+    //     clearInterval(this.timer);
+    // }
 
 
 
     render() {
         const { status, timeElapsed } = this.state;
         return (
-            <div>
-            <p>{this.millisecondConversion(timeElapsed)}ms</p>
-            <button onClick={this.handleStopStart}>{status ? 'Stop' : 'Start'}</button>
-            <button onClick={this.handleLapReset}>Reset</button>
-            </div>
+            <>
+                <p>{this.millisecondConversion(timeElapsed)}ms</p>
+                <button >{status && timeElapsed > 0 ? 'Lap' : 'Reset'}</button>
+                <button >{status ? 'Stop' : 'Start'}</button>
+                {this.state.laps.map(lap => <ul>
+                    <li>{this.millisecondConversion(lap)}</li>
+                </ul>)}
+            </>
         )
     }
 }
