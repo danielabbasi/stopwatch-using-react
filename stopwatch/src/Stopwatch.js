fc6c72a32ca9e0ce09d442255cd47e017adc1b17
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-09 10:10:57 +0100

    Added state to Stopwatch and created tick function that sets new state date. Used Lifecyle method componentdidMount to set interval to update state every second

--- stopwatch/src/Stopwatch.js
+++ stopwatch/src/Stopwatch.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
 
 class Stopwatch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()}
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+
+
     render() {
         return (
             <div>
             <p>0ms</p>
+            <p>It is {this.state.date.toLocaleTimeString()}</p>
             <button>Start</button>
             <button>Reset</button>
             </div>
