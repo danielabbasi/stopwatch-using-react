fb0711f3ceb19b4a5ce4c8faaf4dd6f17c92a8de
Daniel Abbasi <daniel_abbasi@hotmail.co.uk>
2019-09-12 09:49:41 +0100

    code cleanup

--- stopwatch/src/Stopwatch.js
+++ stopwatch/src/Stopwatch.js
@@ -3,26 +3,26 @@ import './App.css';
 
 class Stopwatch extends Component {
     state = {
-        status: false,
+        isRunning: false,
         timeElapsed: 0,
         currentTime: 0,
         lapTimes: []
     }
 
     handleStopStart = () => {
-        if (this.state.status) {
+        if (this.state.isRunning) {
             clearInterval(this.timer);
-            this.setState({ status: false })
+            this.setState({ isRunning: false })
         } else {
             this.timer = setInterval(() => {
                 this.setState({ timeElapsed: this.state.timeElapsed + 1 }); // updater function timer that accepts the current state and returns new state with updated time. this is because state is immutable
             }, 10);
-            this.setState({ status: true })
+            this.setState({ isRunning: true })
         }
     }
 
     handleLapReset = () => {
-        if (this.state.status === false) {
+        if (this.state.isRunning === false) {
             this.timer = this.setState({ timeElapsed: 0, lapTimes: [], currentTime: 0 });
         } else {
             if (this.state.lapTimes.length === 0) {
@@ -32,7 +32,7 @@ class Stopwatch extends Component {
                 })
             } else {
                 const difference = this.state.timeElapsed - this.state.currentTime;
-                this.setState((state) => {
+                this.setState((state,props) => {
                     state.lapTimes.push(difference);
                 })
                 this.setState({ currentTime: this.state.timeElapsed })
@@ -67,17 +67,17 @@ class Stopwatch extends Component {
     }
 
     componentWillUnmount() {
-        clearInterval(this.timer);
+        clearInterval(this.timer);  // when component is to be removed from the DOM, unmounting called and clears interval to avoid memory leak 
     }
 
     render() {
-        const { status, timeElapsed, lapTimes } = this.state;
+        const { isRunning: isRunning, timeElapsed, lapTimes } = this.state;
         return (
             <>
                 <p className="timer">{this.millisecondConversion(timeElapsed)}</p>
                 <div className="container">
-                    <button className={status ? "resetBtn" : "lapBtn"} onClick={this.handleLapReset}>{status && timeElapsed > 0 ? 'Lap' : 'Reset'}</button>
-                    <button className={status ? "stopBtn" : "startBtn"} onClick={this.handleStopStart} >{status ? 'Stop' : 'Start'}</button>
+                    <button className={isRunning ? "resetBtn" : "lapBtn"} onClick={this.handleLapReset}>{isRunning && timeElapsed > 0 ? 'Lap' : 'Reset'}</button>
+                    <button className={isRunning ? "stopBtn" : "startBtn"} onClick={this.handleStopStart} >{isRunning ? 'Stop' : 'Start'}</button>
                 </div>
                 <table>
                     <tbody>
